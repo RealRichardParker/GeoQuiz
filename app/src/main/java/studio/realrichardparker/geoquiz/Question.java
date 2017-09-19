8f48ea25501580755bffee5813987e42a86085b4
Tiger <tiger.c.zhang@gmail.com>
2017-09-19 00:39:36 -0500

    First bits, Question, QuizActivity, controller, view, etc

---
+++ app/src/main/java/studio/realrichardparker/geoquiz/Question.java
@@ -0,0 +1,33 @@
+package studio.realrichardparker.geoquiz;
+
+/**
+ * Created by Tiger on 9/18/2017.
+ */
+
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int mTextResId, boolean mAnswerTrue) {
+
+        this.mTextResId = mTextResId;
+        this.mAnswerTrue = mAnswerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
